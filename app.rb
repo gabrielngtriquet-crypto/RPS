069497230a27566b9f9cd86d83643d106f492851
gabrielngtriquet-crypto <gabrielngtbusiness@gmail.com>
2024-10-23 17:59:37 +0000

    First commit

--- app.rb
+++ app.rb
@@ -7,3 +7,24 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+moves = ["rock", "paper", "scissors"]
+
+@comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+
+elsif @comp_move == "scissors"
+  @outcome = "won"
+
+else 
+    @outcome = "lost"
+  
+end
+
+erb:zebra
+
+end
